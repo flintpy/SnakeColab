42c36b1ffab1460d5bb9575be3ee74d84a2129f3
flintpy <122540606+flintpy@users.noreply.github.com>
2023-08-23 17:19:49 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,102 +1,199 @@
-class SnakeGame {
+class AdvancedSnakeGame {
+    /**
+     * Construct the snake game with initial settings
+     * @param {*} difficulty - Difficulty level of the snake game
+     */
     constructor(difficulty) {
-        this.gridSize = 20;
-        this.snake = [{ x: 10, y: 10 }];
-        this.food = this.generateFood();
-        this.direction = 'right';
-        this.gameBoard = document.getElementById('game-board');
-        this.scoreBoard = document.getElementById('score');
-        this.difficulty = this.setDifficulty(difficulty);
-        this.score = 0;
-        this.interval = setInterval(() => this.tick(), this.difficulty);
+        // Game's configurations
+        this.config = {
+            gridSize: 20,
+            easySpeed: 200,
+            mediumSpeed: 100,
+            hardSpeed: 50,
+            startingPoint: { x: 10, y: 10 },
+            startingScore: 0,
+            directionMap: {
+                ArrowLeft: 'left',
+                ArrowRight: 'right',
+                ArrowUp: 'up',
+                ArrowDown: 'down'
+            },
+            oppositeDirections: {
+                'left': 'right',
+                'right': 'left',
+                'up': 'down',
+                'down': 'up'
+            }
+        };
+
+        // Game's state
+        this.state = {
+            snake: [ this.config.startingPoint ],
+            food: null,
+            direction: 'right',
+            score: this.config.startingScore,
+        };
+
+        // Game's environment
+        this.env = {
+            gameBoard: document.getElementById('game-board'),
+            scoreBoard: document.getElementById('score'),
+            difficulty: this.setDifficulty(difficulty),
+        };
+
+        this.env.interval = setInterval(() => this.tick(), this.env.difficulty);
+
+        // Generate food after setting up all other things
+        this.state.food = this.generateFood();
+
         document.addEventListener('keydown', (e) => this.handleKeydown(e));
+
         this.render(); // Initial render
     }
 
+    /**
+     * Function to set difficulty of the game based on user preference
+     * @param {*} difficulty - Difficulty level set by user, default to medium
+     */
     setDifficulty(difficulty) {
         switch (difficulty) {
-            case 'easy': return 200;
-            case 'medium': return 100;
-            case 'hard': return 50;
-            default: return 100;
+            case 'easy': return this.config.easySpeed;
+            case 'medium': return this.config.mediumSpeed;
+            case 'hard': return this.config.hardSpeed;
+            default: return this.config.mediumSpeed;
         }
     }
 
+    /**
+     * Function to generate food randomly inside the grid.
+     */
     generateFood() {
-        let x, y;
+        let newFood = {};
         do {
-            x = Math.floor(Math.random() * this.gridSize);
-            y = Math.floor(Math.random() * this.gridSize);
-        } while (this.snake.some(part => part.x === x && part.y === y));
-        return { x, y };
+            newFood.x = Math.floor(Math.random() * this.config.gridSize);
+            newFood.y = Math.floor(Math.random() * this.config.gridSize);
+        } while (this.state.snake.some(part => part.x === newFood.x && part.y === newFood.y));
+        return newFood;
     }
 
-    move() {
-        const head = { ...this.snake[0] };
-        switch (this.direction) {
-            case 'left': head.x = (head.x - 1 + this.gridSize) % this.gridSize; break;
-            case 'right': head.x = (head.x + 1) % this.gridSize; break;
-            case 'up': head.y = (head.y - 1 + this.gridSize) % this.gridSize; break;
-            case 'down': head.y = (head.y + 1) % this.gridSize; break;
+    /**
+     * Function to move the snake according to its direction
+     */
+    moveSnake() {
+        const newHead = { ...this.state.snake[0] };
+        switch (this.state.direction) {
+            case 'left': 
+                newHead.x = (newHead.x - 1 + this.config.gridSize) % this.config.gridSize; 
+                break;
+            case 'right': 
+                newHead.x = (newHead.x + 1) % this.config.gridSize; 
+                break;
+            case 'up':
+                newHead.y = (newHead.y - 1 + this.config.gridSize) % this.config.gridSize;
+                break;
+            case 'down': 
+                newHead.y = (newHead.y + 1) % this.config.gridSize; 
+                break;
         }
 
-        if (this.snake.some(part => part.x === head.x && part.y === head.y)) {
-            clearInterval(this.interval); // Game over if snake collides with itself
-            alert('Game Over! Score: ' + this.score);
-            return;
-        }
+        // Check for snake collision
+        this.checkCollision(newHead);
 
-        this.snake.unshift(head);
-
-        if (head.x === this.food.x && head.y === this.food.y) {
-            this.score += 10;
-            this.scoreBoard.textContent = this.score;
-            this.food = this.generateFood();
+        this.state.snake.unshift(newHead);
+        
+        if(this.isFoodEaten(newHead)){
+            this.updateScore();
+            this.state.food = this.generateFood();
         } else {
-            this.snake.pop();
+            this.state.snake.pop();
         }
+        
     }
 
+    /**
+     * Function to check if snake collides with itself or with the boundary
+     * @param {*} head - Current head of the snake
+     */
+    checkCollision(head){
+        if (this.state.snake.some(part => part.x === head.x && part.y === head.y)) {
+            clearInterval(this.env.interval); // Game over if snake collides with itself
+            alert('Game Over! Score: ' + this.state.score);
+            // throw new Error('Game Over. Snake collided with itself.'); or any other preferred way to end the game
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Function that will be called for every defined interval
+     */
     tick() {
-        this.move();
+        this.moveSnake();
         this.render();
     }
 
+    /**
+     * Function to handle user inputs generated by keyboard
+     * @param {*} event - Event generated by a keyboard
+     */
     handleKeydown(event) {
-        const oppositeDirections = {
-            'left': 'right',
-            'right': 'left',
-            'up': 'down',
-            'down': 'up'
-        };
-        const newDirection = event.key.replace('Arrow', '').toLowerCase();
-        if (newDirection !== oppositeDirections[this.direction]) {
-            this.direction = newDirection;
+        const newDirection = this.config.directionMap[event.key];
+        const isNotOppositeDirection = newDirection !== this.config.oppositeDirections[this.state.direction];
+
+        // Only allows to turn right or left 
+        // Not allows to go in opposite direction straight away
+        // Prevents direct reverse
+        if (isNotOppositeDirection) {
+            this.state.direction = newDirection;
         }
     }
 
+    /**
+     * Function to render the snake and food on game board
+     */
     render() {
-        this.gameBoard.innerHTML = '';
+        this.env.gameBoard.innerHTML = '';
 
         // Render snake
-        this.snake.forEach(part => {
-            const snakePartElement = document.createElement('div');
-            snakePartElement.style.left = part.x * 20 + 'px';
-            snakePartElement.style.top = part.y * 20 + 'px';
-            snakePartElement.className = 'snake-part';
-            this.gameBoard.appendChild(snakePartElement);
-        });
+        this.state.snake.forEach(part => this.renderElement(part, 'snake-part'));
 
         // Render food
-        const foodElement = document.createElement('div');
-        foodElement.style.left = this.food.x * 20 + 'px';
-        foodElement.style.top = this.food.y * 20 + 'px';
-        foodElement.className = 'food';
-        this.gameBoard.appendChild(foodElement);
+        this.renderElement(this.state.food, 'food');
+    }
+
+    /**
+     * Function to create and render game element (snake's part or food)
+     * @param {*} element object that contains x and y position
+     * @param {*} className class name to be added for the style
+     */
+    renderElement(element, className) {
+        const newElement = document.createElement('div');
+        newElement.style.left = `${element.x * 20}px`;
+        newElement.style.top = `${element.y * 20}px`;
+        newElement.classList.add(className);
+        this.env.gameBoard.appendChild(newElement);
+    }
+
+    /**
+     * Function to check if the food is eaten
+     * @param {*} head coordinates of the head of snake
+     * @returns boolean- true if food is eaten else false
+     */
+    isFoodEaten(head){
+        return head.x === this.state.food.x && head.y === this.state.food.y;
+    }
+
+    /**
+     * function to update score and update score in frontend
+     */
+    updateScore(){
+        this.state.score += 10;
+        this.env.scoreBoard.textContent = this.state.score;
     }
 }
 
+// Instantiating the game
 document.getElementById('start-button').addEventListener('click', () => {
     const difficulty = document.getElementById('difficulty').value;
-    new SnakeGame(difficulty);
+    new AdvancedSnakeGame(difficulty);
 });
